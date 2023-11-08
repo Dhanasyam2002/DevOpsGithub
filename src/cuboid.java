28f5e37ea04146eb4db420b4d9abc98e8d4570dd
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 05:32:07 +0530

    This is the First Commit .....written only methods...

---
+++ src/cuboid.java
@@ -0,0 +1,19 @@
+
+public class cuboid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		class areas {
+			public float cuboid()
+			{
+				return 0;
+				
+			}
+			
+
+		}
+
+	}
+
+}
+
---
+++ src/rectangle.java
@@ -0,0 +1,19 @@
+
+public class rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		class areas {
+			public float rectangle()
+			{
+				return 0;
+				
+			}
+			
+
+		}
+
+	}
+
+}
+
