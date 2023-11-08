c4d17edb968e7c4660713c034b8faf7739deb68f
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 12:52:51 +0530

    added method to cuboid

--- src/cuboid.java
+++ src/cuboid.java
@@ -4,9 +4,11 @@ public class cuboid {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		class areas {
-			public float cuboid()
+			public float cuboid(float l,float b,float h)
 			{
-				return 0;
+				 float area = 2 (lb + bh + lh);
+				 System.out.Println("Area of Cuboid : "+area);
+				 return area;
 				
 			}
 			
