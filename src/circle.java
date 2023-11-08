9c1e32b083344b5c6010c32b0bb6c12a547d50d4
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 07:19:53 +0530

    method return for circle

--- src/circle.java
+++ src/circle.java
@@ -3,6 +3,24 @@ public class circle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		class Circle {
+
+		    private double radius;
+
+		    public Circle(double radius) {
+		        this.radius = radius;
+		    }
+
+		    // standard getter and setter
+
+		    private double calculateArea() {
+		        return radius * radius * Math.PI;
+		    }
+
+		    public String toString() {
+		        return "The area of the circle [radius = " + radius + "]: " + calculateArea();
+		    }
+		}
 
 	}
 
